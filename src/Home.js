631f4323ef0dce7ba0a889779fd494bb3c3af822
Eoin <eoin.magualghairg@gmail.com>
2023-12-10 21:50:58 +0000

    Home media queries

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ function Home() {
             <div className="first-item">
                 <div className='text'>
                     <div className='name-big'>
-                        <p>Eoin Proinsias Rhisiart</p>
-                        <p>Mag Ualghairg</p>
+                        <p className='first-name'>Eoin Proinsias Rhisiart</p>
+                        <p className='last-name'>Mag Ualghairg</p>
                     </div>
 
                     <div className='blurb'>
@@ -15,15 +15,11 @@ function Home() {
                             I'm a 6th form student in my last year at <a href='https://www.glantaf.cymru/'>Ysgol Gyfun Gymraeg Glantaf</a>.
                             I study Mathematics, Further Mathematics, Physics, and Computer Science.
                         </p>
-                        <p>
-                            In my spare time I play 5-aside football, code, and socialise with my friends.
-                        </p>
                     </div>
                 </div>
 
                 <img src={require('./src/portrait.jpeg')} className="portrait-image"/>
 
-                
             </div>
         </div>
     );
