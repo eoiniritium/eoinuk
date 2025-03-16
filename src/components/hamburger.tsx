9f959f3616a516e24d313279d242825134a38d31
Eoin <eoin.magualghairg@gmail.com>
2025-03-16 18:41:26 +0000

    Mobile friendly Header

--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -2,20 +2,18 @@
 
 import { useState } from "react";
 
-export default function Hamburger({className, children}: {className: string, children: React.ReactNode}) {
-    const [openHamburger, setOpenHamburger] = useState(false);
+interface HamburgerProps {
+    hamburgerButtonJSX: React.ReactNode;
+    children: React.ReactNode;
+}
 
-    const cn = "group inline-flex text-center items-center justify-center " + className;
+export default function Hamburger({hamburgerButtonJSX, children}: HamburgerProps) {
+    const [openHamburger, setOpenHamburger] = useState(false);
 
     return (
     <>
-    <button onClick={() => {setOpenHamburger(!openHamburger); }} aria-pressed={openHamburger} className={cn}>
-    <span className="sr-only">Menu</span>
-    <svg className="w-6 h-6 fill-current pointer-events-none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-        <rect className="origin-center -translate-y-[5px] translate-x-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-x-0 group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[315deg]" y="7" width="9" height="2" rx="1"></rect>
-        <rect className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-[[aria-pressed=true]]:rotate-45" y="7" width="16" height="2" rx="1"></rect>
-        <rect className="origin-center translate-y-[5px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[135deg]" y="7" width="9" height="2" rx="1"></rect>
-    </svg>
+    <button onClick={() => {setOpenHamburger(!openHamburger); }}>
+        {hamburgerButtonJSX}
     </button>
 
     {
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa";
 import Hamburger from "./hamburger";
 
 export default function Header() {
     return(
-    <div className="relative bg-blue grain text-white px-10 py-2 w-full flex items-center justify-between">
-        <h4 className="max-sm:hidden"><Link href="/">Eoin</Link></h4>
-        <div className="collapse max-sm:visible">
-            <Hamburger className='rounded text-blue py-1 px-4 bg-white'>
-                <div className="py-2">
-                    <h3><Link href="/" className="link">Home</Link></h3>
-                    <h3><Link href="/projects" className="link">Projects</Link></h3>
-                    <h3><Link href="/tutoring" className="link">Tutoring</Link></h3>
-                </div>
-            </Hamburger>
+    <div className="relative bg-blue grain text-white px-3 sm:px-10 py-2 w-full flex items-center justify-between">
+        <div className="flex items-center">
+            <h4><Link href="/" className="block max-sm:hidden">Eoin</Link></h4>
+            <div className="hidden max-sm:block">
+                <Hamburger hamburgerButtonJSX={
+                    <div className='px-3 py-3 flex items-center'>
+                        <h4>Eoin</h4>
+                        <FaCaretDown className="ml-2"/>
+                    </div>
+                }>
+                    <div className="py-2 pl-3 pr-10 flex flex-col h-40 justify-around absolute bg-blue grain p-5 rounded-b">
+                        <h3><Link href="/" className="link">Home</Link></h3>
+                        <h3><Link href="/projects" className="link">Projects</Link></h3>
+                        <h3><Link href="/tutoring" className="link">Tutoring</Link></h3>
+                    </div>
+                </Hamburger>
+            </div>
         </div>
         
         <div className="max-sm:hidden flex justify-around w-full max-w-96">
@@ -22,7 +30,7 @@ export default function Header() {
             <Link href="/tutoring" className="link">Tutoring</Link>
         </div>
             
-        <div className="flex justify-between h-full pt-[0.4rem]">
+        <div className="flex justify-between">
             <h4 className="pr-1"><a target="_blank"  href="https://github.com/eoiniritium/"><FaGithub/></a></h4>
             <h4 className="px-1"><a target="_blank"  href="https://www.linkedin.com/in/eoin-mag-ualghairg-12485722b/"><FaLinkedin/></a></h4>
             <h4 className="pl-1"><a target="_blank"  href="https://www.instagram.com/eoiniritium/"><FaInstagram/></a></h4>
