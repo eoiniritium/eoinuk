1b937602ccce0667b61358724718dcb93a25ab04
Eoin <eoin.magualghairg@gmail.com>
2025-03-16 02:03:21 +0000

    Updated Header

---
+++ src/components/hamburger.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from "react";
+
+export default function Hamburger({className, children}: any) {
+    const [openHamburger, setOpenHamburger] = useState(false);
+
+    const cn = "group inline-flex text-center items-center justify-center " + className;
+
+    return (
+    <>
+    <button onClick={() => {setOpenHamburger(!openHamburger); }} aria-pressed={openHamburger} className={cn}>
+    <span className="sr-only">Menu</span>
+    <svg className="w-6 h-6 fill-current pointer-events-none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        <rect className="origin-center -translate-y-[5px] translate-x-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-x-0 group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[315deg]" y="7" width="9" height="2" rx="1"></rect>
+        <rect className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-[[aria-pressed=true]]:rotate-45" y="7" width="16" height="2" rx="1"></rect>
+        <rect className="origin-center translate-y-[5px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[135deg]" y="7" width="9" height="2" rx="1"></rect>
+    </svg>
+    </button>
+
+    {
+        openHamburger &&
+        <div>{children}</div>
+    }
+    </>
+    );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
 import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa6";
+import Hamburger from "./hamburger";
 
 export default function Header() {
     return(
-    <div className="bg-blue grain text-white px-10 py-2 w-full flex items-center justify-between">
-        <h4><Link href="/" className="font-size">Eoin</Link></h4>
-        <div className="flex justify-around w-full max-w-96">
+    <div className="relative bg-blue grain text-white px-10 py-2 w-full flex items-center justify-between">
+        <h4 className="max-sm:hidden"><Link href="/">Eoin</Link></h4>
+        <div className="collapse max-sm:visible">
+            <Hamburger className='rounded text-blue py-1 px-4 bg-white'>
+                <div className="py-2">
+                    <h3><Link href="/" className="link">Home</Link></h3>
+                    <h3><Link href="/projects" className="link">Projects</Link></h3>
+                    <h3><Link href="/tutoring" className="link">Tutoring</Link></h3>
+                </div>
+            </Hamburger>
+        </div>
+        
+        <div className="max-sm:hidden flex justify-around w-full max-w-96">
             <Link href="/" className="link">Home</Link>
             <Link href="/projects" className="link">Projects</Link>
             <Link href="/tutoring" className="link">Tutoring</Link>
         </div>
             
-        <div className="flex justify-between">
+        <div className="flex justify-between h-full pt-[0.4rem]">
             <h4 className="pr-1"><a target="_blank"  href="https://github.com/eoiniritium/"><FaGithub/></a></h4>
             <h4 className="px-1"><a target="_blank"  href="https://www.linkedin.com/in/eoin-mag-ualghairg-12485722b/"><FaLinkedin/></a></h4>
             <h4 className="pl-1"><a target="_blank"  href="https://www.instagram.com/eoiniritium/"><FaInstagram/></a></h4>
